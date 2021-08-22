00e6705a619cbf02eb34d450118598fa107025df
YeshayahuBennun <ybennun@yahoo.com>
2021-08-22 11:46:57 +0300

    commit#6 - 18. Defining a Task Model

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './tasks.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Task[] = [];
 
-  getAllTask() {
+  getAllTask(): Task[] {
     return this.tasks;
   }
 }
