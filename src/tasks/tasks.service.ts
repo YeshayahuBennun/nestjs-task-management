0433d922f8ae73c595eeab5b42119494f6df6b0d
YeshayahuBennun <ybennun@yahoo.com>
2021-08-22 11:08:50 +0300

    commit#5 - 15. Feature: Getting All Tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks = [];
+
+  getAllTask() {
+    return this.tasks;
+  }
+}
