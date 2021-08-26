bc0975216e2b842cd5acc769aff50bc1cf64c237
YeshayahuBennun <ybennun@yahoo.com>
2021-08-26 17:38:04 +0300

    commit#16 - 33. Error Handling: Deleting a non-existing Task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -65,7 +65,8 @@ export class TasksService {
   }
 
   deleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id == id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id == found.id);
   }
 
   updateTask(id: string, status: TaskStatus) {
