08cf45fa316f79de7bbb4ef3290068bed84e79f2
YeshayahuBennun <ybennun@yahoo.com>
2021-08-31 16:55:15 +0300

    commit#32 - 59. Validation: Credentials and Password Strength

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,24 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+  @IsString()
+  @MinLength(8)
+  @MaxLength(22)
+
+  /*strong-password-regex.md
+Passwords will contain at least 1 upper case letter
+Passwords will contain at least 1 lower case letter
+Passwords will contain at least 1 number or special character
+There is no length validation (min, max) in this regex!
+Regular expression for JavaScript:
+---------------------------------------------------------------
+/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/*/
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak',
+  })
   password: string;
 }
