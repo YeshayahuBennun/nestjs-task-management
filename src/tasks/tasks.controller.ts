0788a52250e5601bc732b8c7e94f6d769740f3ae
YeshayahuBennun <ybennun@yahoo.com>
2021-09-06 13:12:44 +0300

    commit#47 - Implementing Logs in a NestJS app

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -17,10 +17,13 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-tasks-status.dto';
 import { Task } from './Task.entity';
 import { TasksService } from './tasks.service';
+import { Logger } from '@nestjs/common';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger('TasksController');
+
   constructor(private taskService: TasksService) {}
 
   @Get()
@@ -28,6 +31,11 @@ export class TasksController {
     @Query() filterDto: GetTasksFilterDto,
     @GetUser() user: User,
   ): Promise<Task[]> {
+    this.logger.verbose(
+      `User "${user.username}" retrieving all tasks. Filters: ${JSON.stringify(
+        filterDto,
+      )}`,
+    );
     return this.taskService.getTasks(filterDto, user);
   }
 
@@ -41,6 +49,11 @@ export class TasksController {
     @Body() createTaskDto: CreateTaskDto,
     @GetUser() user: User,
   ): Promise<Task> {
+    this.logger.verbose(
+      `User "${user.username}" creating a new task. Data: "${JSON.stringify(
+        createTaskDto,
+      )}"`,
+    );
     return this.taskService.createTask(createTaskDto, user);
   }
 
