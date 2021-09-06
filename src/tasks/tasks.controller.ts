ace7616b7188c8141b68b7ab99d2a08fcfebc4da
YeshayahuBennun <ybennun@yahoo.com>
2021-09-06 11:13:36 +0300

    commit#44 - Restricting Getting All Tasks by User

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -24,8 +24,11 @@ export class TasksController {
   constructor(private taskService: TasksService) {}
 
   @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto): Promise<Task[]> {
-    return this.taskService.getTasks(filterDto);
+  getTasks(
+    @Query() filterDto: GetTasksFilterDto,
+    @GetUser() user: User,
+  ): Promise<Task[]> {
+    return this.taskService.getTasks(filterDto, user);
   }
 
   @Get('/:id')
